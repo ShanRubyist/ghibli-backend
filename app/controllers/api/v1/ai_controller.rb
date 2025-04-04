606b71ba8fe390f8ad96b9225f0f41f1f9f409ce
shan <2302531309@qq.com>
2025-04-04 17:07:17 +0800

    Update ai_controller.rb

--- app/controllers/api/v1/ai_controller.rb
+++ app/controllers/api/v1/ai_controller.rb
@@ -1,54 +1,14 @@
 require 'bot'
 
 class Api::V1::AiController < UsageController
-  # skip_before_action :check_credits, only: [:ai_call_info]
-
-  def current_user
-    User.first
-  end
-
+  skip_before_action :check_credits, only: [:ai_call_info]
+  
   def gen_image
-    prompt = params['prompt'] || '1GHBLI anime style photo'
-    raise 'prompt can not be empty' unless prompt.present?
-
-    image = params['image'] || 'https://replicate.delivery/pbxt/Mjo5VWBA0fm8oq3BefpbdtXeJitBJc6AobPnnMdFxxCKzT3P/linkedin.jpeg'
-
-    conversation = current_user.conversations.create
-
-    # generate video task
-    # ai_bot = Bot::Replicate.new
-    # image = ai_bot.generate_image(prompt, image: image, model_name: 'aaronaftab/mirage-ghibli')
-
-    ai_call = conversation.ai_calls.create(
-      task_id: SecureRandom.uuid,
-      prompt: prompt,
-      status: 'submit',
-      input: params,
-      "cost_credits": current_cost_credits)
-    image="https://replicate.delivery/xezq/DffSSwyLWeEh3ocTwn6qzOzOcREmqRFDqhmV3zAzk7IH6e8RB/out-0.webp"
-    # OSS
-    require 'open-uri'
-    # ai_call= AiCall.last
-    ai_call.generated_media.attach(io: URI.open(image),
-                                   filename: URI(image).path.split('/').last,
-                                   content_type: "image/jpeg")
-    medias = ai_call.generated_media
-    puts '*'*100
-    puts medias
-
-    render json: {
-      images: (
-        medias.map do |i|
-          url_for(i)
-        end
-      )
-    }
-  end
-  def gen_image2
     prompt = params['prompt'] || 'GHBLI anime style photo'
     raise 'prompt can not be empty' unless prompt.present?
 
     image = params['image']
+    raise 'iamge can not be empty' unless image.present?
 
     conversation = current_user.conversations.create
 
@@ -65,8 +25,9 @@ class Api::V1::AiController < UsageController
       status: 'submit',
       input: params,
       "cost_credits": current_cost_credits)
-    
+
     # OSS
+    # TODO: jobs
     require 'open-uri'
     ai_call.generated_media.attach(io: URI.open(image),
                                    filename: URI(image).path.split('/').last,
